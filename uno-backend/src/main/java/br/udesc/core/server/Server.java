1b9b5ca454de898dd5fe6287f4bfde9787fb3f25
murilogoedert <murilogoedert@gmail.com>
2022-11-24 20:43:55 -0300

    Ajustes

--- uno-backend/src/main/java/br/udesc/core/server/Server.java
+++ uno-backend/src/main/java/br/udesc/core/server/Server.java
@@ -2,17 +2,32 @@ package br.udesc.core.server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import br.udesc.util.Config;
 
 public class Server {
 
     private Logger logger;
+    private Map<Integer, ClientSocketThread> clients;
+    private List<ClientSocketThread> clientSockets;
 
     public Server() {
         this.logger = Logger.getLogger("ServerLogger");
+        this.clients = new HashMap<>();
+        this.clientSockets = new ArrayList<>();
+    }
+
+    public void bindUser(int userId, ClientSocketThread socketClient){
+        this.clients.put(userId, socketClient);
+    }
+
+    public ClientSocketThread getClientSocket(int userId){
+        return clients.get(userId);
     }
 
     public void startServer() {
@@ -26,22 +41,16 @@ public class Server {
 
             try {
                 while (true) {
-
                     socket = serverSocket.accept();
                     logger.log(Level.INFO, "Conexão com o cliente " + socket.getLocalAddress() + " efetuada!");
 
                     //Cria uma Thread para o cliente
                     ClientSocketThread clientThread = new ClientSocketThread(socket);
+                    clientSockets.add(clientThread);
+
 
                     //Adiciona um listener para as mensagens vindas do cliente
-                    clientThread.setListener(new ClientSocketListener() {
-
-                        @Override
-                        public void onMessage(String message) {
-                            logger.info("O cliente enviou uma mensagem: " + message);
-                        }
-
-                    });
+                    clientThread.setListener(MessageBroker.getInstance());
 
                     //Inicia a thread do cliente
                     clientThread.start();
